19de8b290e126690d1ea1fcf14bdc3df26155018
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-16 12:44:52 -0400

    Update dice 7 to 5 - G interview.py

--- dice 7 to 5 - G interview.py
+++ dice 7 to 5 - G interview.py
@@ -160,4 +160,67 @@ len(outcomes)
 6//6 #1
 35//6#5
 rand_1_7() 
+
+  
+'''
+#Given a dice which roll from 1 t o 5, simulate a uniform 7 sided dice
+#because the 5sided dice can not produce 7possible outcomes on a single roll,
+we immediately know that we need to
+#roll the dice at least twice. If we roll the dice twice we have 25 possible combinations of 
+the results of the two 
+#rolls. While 25 is not divisible by 7, 21 is. This means we can implment our previous strategy 
+of throwing out
+#rolls not in our intended range. It's also important to note that we can't expand the solution 
+to implment more rolls
+#in order to not throw any out, because 5 and 7 are both prime which menas that no exponent of 
+5 will be divisible by 7 no matter how high you go
+#We will define our range as a section of the 25 possible combinations of rolls. A good way to 
+do this is by converting the two rolls into a unique outcome number in the range 1 through 25. 
+#we will create this number by taking the rolls, then we take the first roll and 
+after subtacting 1 from it we multiply
+#it by 5. Now the first roll corresponds with a value of 1-20(0,5,10,15,20). 
+Then we take the second roll and add it to the result 
+#of the first manipulation. Giving us a range of 1-25. So our final solution is to roll the dice twice.
+ Check the 
+#manipulated range from 1 to 25, if its greater than 21, do a reroll. Let's see it in actaion. 
+'''
+from random import randint
+def dice5():
+    return randint(1,5)
+
+def convert5to7():
+    #for constant re-roll purposes.
+    while True:
+        #roll the dice twice
+        roll_1=dice5()
+        roll_2=dice5()
+        
+        #print 'The rolls were {} and {}'.format(roll_1,roll_2)
+        
+        #convert the combination to the range 1 to 25
+        num=((roll_1-1)*5)+(roll_2)
+        
+        #print 'The converted range number was:",num
+#        if num>21:
+#            #re-roll if we are out of range
+#            continue
+#        return num%7+1
     
+        if num>21:#why 25 cut? there is no 26, 27, 28 with residuals to be pared with previous paris
+            roll_1=dice5()#BK
+            roll_2=dice5()#BK
+            num=((roll_1-1)*5)+(roll_2)
+        else:
+            return num%7+1
+    
+    
+    
+convert5to7()
+
+ans = [] 
+for _ in range(100000): 
+    ans.append(convert5to7()) 
+sns.distplot(ans,kde=False) 
+
+15%7
+15//7
