b66fea0b30dc834bc406f0c156e39606980114e6
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-16 13:43:24 -0400

    Update product except at that index.py

--- product except at that index.py
+++ product except at that index.py
@@ -57,3 +57,46 @@ def index_prod(nums):
         postfix*=nums[i]
     return res
 index_prod([1,2,3,4])
+
+
+
+
+
+
+'''
+Product of Array Except Self
+
+Given an integer array nums, return an array answer such that answer[i] 
+is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+
+
+'''
+    def productExceptSelf(nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        output = [None]*len(nums)
+        product = 1
+        i=0
+        
+        while i<len(nums):
+            output[i] = product
+            product *= nums[i]
+            i+=1
+            
+        product = 1
+        
+        i=len(nums)-1
+        while i>=0:
+            output[i] *=product
+            product *=nums[i]
+            i-=1
+        return output
+
+
+productExceptSelf([1,2,3,4]) #[24,12,8,6]
+
