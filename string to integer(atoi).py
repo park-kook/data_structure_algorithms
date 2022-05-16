c1b450f731c842b753198a11b577490c2d72daf1
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-16 13:16:11 -0400

    Update string to integer(atoi).py

--- string to integer(atoi).py
+++ string to integer(atoi).py
@@ -19,7 +19,7 @@ Note:
 Only the space character ' ' is considered a whitespace character.
 Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
 
-  
+  '''
 #############################
     def myAtoi(s: str) -> int:
         result = ''
