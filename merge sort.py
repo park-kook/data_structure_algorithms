1748fc99ececa03746e76c68c3f16f35fe6fe0bd
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-16 13:37:46 -0400

    Update and rename merge sort to merge sort.py

--- merge sort.py
+++ merge sort.py
@@ -1,3 +1,43 @@
+
+
+'''
+Merge Sorted Array
+
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that 
+should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+'''
+
+
+
+    def merge(nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        # Write the elements of num2 into the end of nums1.
+        for i in range(n):
+            nums1[i + m] = nums2[i]
+        # Sort nums1 list in-place.
+            nums1.sort()   
+        return nums1
+
+nums1=[1,2,3,0,0,0]
+m=3
+nums2=[2,5,6]
+n=3
+
+merge([1,2,3,0,0,0], 3, [2,5,6],3)
+merge([1], 1, [],0)
+merge([0], 0, [1],1)
+
+
 # Python program for implementation of MergeSort
 def mergeSort(arr):
     if len(arr) > 1:
