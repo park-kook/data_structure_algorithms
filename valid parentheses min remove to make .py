64d3f86d65bf7a478b69d7a39edc2ec9247bd539
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-16 10:48:43 -0400

    Create valid parentheses min remove to make .py

---
+++ valid parentheses min remove to make .py
@@ -0,0 +1,26 @@
+###another type
+def minRemoveToMakeValid(s):
+    index_to_remove = []
+    stack=[]
+    for index,value in enumerate(s):
+        if value not in '()':
+            continue
+        if value =='(':
+            stack.append(index)
+        else: 
+            if len(stack)==0:
+#         elif not stack:
+                index_to_remove.append(index) #) first case
+#         else: 
+            else: #stack is not empty, there is something there 
+                stack.pop()
+    index_to_remove +=stack
+    string_builder=[]
+    
+    
+    for index,value in enumerate(s):
+        if index not in index_to_remove:
+            string_builder.append(value)
+    return "".join(string_builder)
+
+###
