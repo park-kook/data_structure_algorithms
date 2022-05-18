3dab6bae77d480007c349a682515b60052ececfd
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-18 13:36:21 -0400

    Update merge sort.py

--- merge sort.py
+++ merge sort.py
@@ -25,7 +25,7 @@ should be merged, and the last n elements are set to 0 and should be ignored. nu
         for i in range(n):
             nums1[i + m] = nums2[i]
         # Sort nums1 list in-place.
-            nums1.sort()   
+        nums1.sort()   
         return nums1
 
 nums1=[1,2,3,0,0,0]
@@ -36,7 +36,28 @@ n=3
 merge([1,2,3,0,0,0], 3, [2,5,6],3)
 merge([1], 1, [],0)
 merge([0], 0, [1],1)
+######################
+def mergesort(nums1,m,nums2,n):
+    #last index nums1
+    last = m+n-1
+    
+    #merge in reverse order
+    while m>0 and n>0:
+        if nums1[m-1] > nums2[n-1]:
+            nums1[last] = nums1[m-1]
+            m-=1
+        else: 
+            nums1[last] = nums2[n-1]
+            n-=1
+        last-=1
+    while n>0:
+        nums1[last] = nums2[n-1]
+        n, last = n-1, last-1
+    return nums1
+        
+mergesort([1,2,3,0,0,0], 3, [2,5,6],3)    
 
+######################
 
 # Python program for implementation of MergeSort
 def mergeSort(arr):
