94c3c1973124d953a40169e7e1f883a6dc392f9f
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-20 15:32:55 -0400

    Create Merge k sorted arrays.py

---
+++ Merge k sorted arrays.py
@@ -0,0 +1,17 @@
+ef mergeKArrays(arr,n,k,output):
+    c=0
+    output=[]
+    
+    for i in range(k):
+        for j in range(n):
+            output.append((arr[i][j]))
+#            c+=1
+    output.sort()
+    
+    return output
+
+k=3
+n=4
+arr=[[2,6,12,34], [1,9,20,1000], [23,24, 90, 2000]]
+output = [0 for i in range(n*k)]
+mergeKArrays(arr,n,k, output)
