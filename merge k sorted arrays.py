850a6514e8a05832b89c4f59a95448f1c3ffbe50
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-20 15:33:53 -0400

    Update merge k sorted arrays.py

--- merge k sorted arrays.py
+++ merge k sorted arrays.py
@@ -1,4 +1,13 @@
-ef mergeKArrays(arr,n,k,output):
+'''
+
+Complexity Analysis: 
+Time Complexity :O(n*k*log(n*k)). 
+since resulting array is of N*k size.
+Space Complexity :O(n*k), The output array is of size n*k.
+'''
+
+
+def mergeKArrays(arr,n,k,output):
     c=0
     output=[]
     
@@ -15,3 +24,5 @@ n=4
 arr=[[2,6,12,34], [1,9,20,1000], [23,24, 90, 2000]]
 output = [0 for i in range(n*k)]
 mergeKArrays(arr,n,k, output)
+
+
