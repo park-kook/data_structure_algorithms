7e4003d08e050f3583375b5dd7dbaa51dfbd6413
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-22 13:34:44 -0400

    Create binary Tree Maximum Path Sum.py

---
+++ binary Tree Maximum Path Sum.py
@@ -0,0 +1,44 @@
+
+Binary Tree Maximum Path Sum
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge
+connecting them. A node can only appear in the sequence at most once. Note that path does not need to 
+pass through the root. The  path sum of a path is the sum of the nodes' values nin the path
+Given the root of binary tree, return the maximum path sum of any path
+'''
+root = [1,2,3]
+output = 6
+
+class node:
+    def __init__(self, val = 0):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def maxPathSum(root):
+    res = [root.val]
+    
+    #return max path sum without split
+    def dfs(root):
+        if not root:
+            return 0
+        
+        leftMax = dfs(root.left)
+        rightMax = dfs(root.right)
+        leftMax = max(leftMax,0)
+        rightMax = max(rightMax, 0)
+        
+        #compute max path sum with splits
+        
+        res[0] = max(res[0], root.val + leftMax + rightMax)
+        return root.val + max(leftMax, rightMax)
+    dfs(root)
+    return res[0]
+
+root = None
+root = node(1)
+root.left = node(2)
+root.right = node(3)
+root.right.left = node(4)
+root.right.right = node(5)
+
+maxPathSum(root)  ##12 = = 3+4+5
