db392a3ea6c5b245b4910b93349f7710ddaa0ae9
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-24 15:27:02 -0400

    Update binary tree max path sum.py

--- binary tree max path sum.py
+++ binary tree max path sum.py
@@ -37,6 +37,9 @@ def maxPathSum(root):
         #1. compute max path sum with splits
         res[0] = max(res[0], root.val + leftMax + rightMax)
         return root.val + max(leftMax, rightMax) #2. without split, we cannot choose both. Choose one of them
+        
+       #between 1 and 2 compete to choose max number for split or without split
+   
     dfs(root) # call dfs function with root input to update global variable res above
     return res[0]
 
