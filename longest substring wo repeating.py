ca62337bb552daeeea8b4ddb5da40926227afc02
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-25 16:42:53 -0400

    Update longest substring wo repeating.py

--- longest substring wo repeating.py
+++ longest substring wo repeating.py
@@ -20,3 +20,28 @@ lengthoflogestsub('abcdeafbdgcbb')#eafbdgc
 
 s = 'pwwkew'
 s='abcdeafbdgcbb'
+
+
+'''
+Longest Substing without repeating characters
+Given a string, find the length of the longest substring without repeating characters
+time complexity O(N)
+space complexity O(1)
+'''
+s = "abcabcbb"
+output=3
+
+def lengthOfLongestSubstring(s):
+    charSet= set()
+    l = 0
+    res = 0
+    
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[r])
+            l+=1
+        charSet.add(s[r])
+        res = max(res, r-l+1)
+    return res
+
+lengthOfLongestSubstring(s)
