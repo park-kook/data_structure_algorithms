3f1f17a966f0a7454e045e48afe97b38ecfdfb57
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-25 17:32:47 -0400

    Update longest repeat char replacement.py

--- longest repeat char replacement.py
+++ longest repeat char replacement.py
@@ -5,6 +5,9 @@ You are given a sting s and an integer k. you can choose any character of the st
 change it to any other uppercase English character. You can perform this operation at most k times
 Return the length of the longest substring containing the same letter 
 you can get after replacing
+
+time complexity O(n)
+space complexity O(1)
 '''
 
 s = "AABABBA"
@@ -20,8 +23,9 @@ def characterreplacement(s,k):
     for r in range(len(s)):
         count[s[r]] = 1+count.get(s[r],0)
         
-        while (r-l+1) - max(count.values()) >k:
-            count[s[l]]-=1
+        while (r-l+1) - max(count.values()) >k: #left pointer moves when more than K replacement
+                                                #compared to the length of the list so far
+            count[s[l]]-=1                      #and left pointer dictionary make -1 
             l+=1
             
         res = max(res, r-1+1)
