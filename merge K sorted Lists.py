9aabdf472670916e85b7eb1b0d35a96e610e58db
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-25 10:37:02 -0400

    Update merge K sorted Lists.py

--- merge K sorted Lists.py
+++ merge K sorted Lists.py
@@ -11,6 +11,12 @@ heapq.heappush(li,4)
 
 heapq.heappop(li)
 1
+
+Time Complexity: O(N * log k) or O(n * k * log k), where, ‘N’ is the total number of elements among all the linked lists, ‘k’ is the total number of lists, and ‘n’ is the size of each linked list.
+Insertion and deletion operation will be performed in min-heap for all N nodes.
+Insertion and deletion in a min-heap require log k time.
+Auxiliary Space: O(k).
+The priority queue will have atmost ‘k’ number of elements at any point of time, hence the additional space required for our algorithm is O(k).
 '''
 
 import heapq
