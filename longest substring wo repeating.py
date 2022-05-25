d48067ac76f6c52d6b57bc8d8ffaaeb3a612c99d
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-25 17:23:15 -0400

    Update longest substring wo repeating.py

--- longest substring wo repeating.py
+++ longest substring wo repeating.py
@@ -1,28 +1,4 @@
 '''
-Longest Substring Without Repeating characters. 
-Given a s tring s, find the length of the longest substing without repeating characters.
-
-'''
-def lengthoflogestsub(s):
-    seen=''
-    ans=0
-    for char in s:
-        if char not in seen:
-            seen+=char
-            ans = max(ans,len(seen))
-        else:
-            cut = seen.index(char)
-            seen=seen[cut+1:]+char                        
-    return ans
-
-lengthoflogestsub('pwwkew')#ske
-lengthoflogestsub('abcdeafbdgcbb')#eafbdgc
-
-s = 'pwwkew'
-s='abcdeafbdgcbb'
-
-
-'''
 Longest Substing without repeating characters
 Given a string, find the length of the longest substring without repeating characters
 time complexity O(N)
@@ -48,3 +24,28 @@ def lengthOfLongestSubstring(s):
 lengthOfLongestSubstring(s)
 s = 'pwwkew'
 output = 3
+
+
+
+'''
+Longest Substring Without Repeating characters. 
+Given a s tring s, find the length of the longest substing without repeating characters.
+
+'''
+def lengthoflogestsub(s):
+    seen=''
+    ans=0
+    for char in s:
+        if char not in seen:
+            seen+=char
+            ans = max(ans,len(seen))
+        else:
+            cut = seen.index(char)
+            seen=seen[cut+1:]+char                        
+    return ans
+
+lengthoflogestsub('pwwkew')#ske
+lengthoflogestsub('abcdeafbdgcbb')#eafbdgc
+
+s = 'pwwkew'
+s='abcdeafbdgcbb'
