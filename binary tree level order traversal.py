e2bf3fea14dbf9bfe1fbcfbaaac4a520df6e1434
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-25 21:58:28 -0400

    Update binary tree level order traversal.py

--- binary tree level order traversal.py
+++ binary tree level order traversal.py
@@ -46,3 +46,39 @@ root.right = Node(20)
 root.right.left = Node(15)
 root.right.right = Node(7)
 levelOrder(root)
+
+
+
+'''
+average sum by level order using queue BDF
+'''
+def levelOrder(root):
+    res = []
+    q = collections.deque()
+    q.append(root)
+    
+    while q:
+        qLen = len(q)
+        level = []
+        for i in range(qLen):
+            node = q.popleft()
+            if node:
+                level.append(node.val)
+                q.append(node.left)
+                q.append(node.right)
+                
+        if level:
+            avg = sum(level)/len(level)
+            res.append(avg)
+#            res.append(level)
+    return res
+
+
+root = None
+root = Node(3)
+root.left = Node(9)
+root.right = Node(20)
+root.right.left = Node(15)
+root.right.right = Node(7)
+levelOrder(root)
+[3.0, 14.5, 11.0]
