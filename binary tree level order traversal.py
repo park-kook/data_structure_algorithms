061ba9474e3e985e4db2885a5ca1ae3d976eebb4
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-25 22:00:17 -0400

    Update binary tree level order traversal.py

--- binary tree level order traversal.py
+++ binary tree level order traversal.py
@@ -1,4 +1,11 @@
+'''
+Binary Tree level order traversal
+Given the root of a binary tree return the level order traversal of its nodes'values 
+(i.e. from left to right level by level)
+time comlplexity: o(N)
+space complexity: o(N)
 
+'''
 import collections
 import heapq
 from collections import deque
