219dd399a1c6b292207c39f6519fa7574b9c6e74
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-25 22:42:18 -0400

    Update two sum - array sorted.py

--- two sum - array sorted.py
+++ two sum - array sorted.py
@@ -3,6 +3,9 @@ Given an array of integers that is already sorted in ascending order. Find
 two numbers such that they add up to a specific target number. 
 The function twoSum should return indices of the two numbers such that they add up to that target,
 where index1 must be less than index2
+
+Time Complexity: O(N).
+Space Complexity: O(1).
 '''
 numbers = [2,7,11,15]
 target = 9
