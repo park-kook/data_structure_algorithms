ac0a8f4fa269288c8193c6552efcd56eb573186e
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-25 22:07:26 -0400

    Update maximum subarray sum.py

--- maximum subarray sum.py
+++ maximum subarray sum.py
@@ -3,13 +3,6 @@ Maximum Subarray
 Given an integer array nums, find the contiguous subarray (containing) at least one number)
     which has the largest sum and return its sum
 
-Time Complexity: O(N * log k) or O(n * k * log k), where, ‘N’ is the total number of elements among 
-all the linked lists, ‘k’ is the total number of lists, and ‘n’ is the size of each linked list.
-Insertion and deletion operation will be performed in min-heap for all N nodes.
-Insertion and deletion in a min-heap require log k time.
-Auxiliary Space: O(k).
-The priority queue will have atmost ‘k’ number of elements at any point of time, hence 
-the additional space required for our algorithm is O(k).    
 
 '''
 
