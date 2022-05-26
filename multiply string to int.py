0cf562f8f3b6d31437193a3d12a0b84f79c3376a
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-26 08:16:47 -0400

    Update multiply string to int.py

--- multiply string to int.py
+++ multiply string to int.py
@@ -54,3 +54,27 @@ multiply("123","456")
         num1, num2=int(num1), int(num2)
         output = num1*num2
         return str(output)
+
+    
+ '''
+ another version
+ '''
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if "0" in [num1, num2]:
+            return "0"
+        
+        res = [0] * (len(num1) + len(num2))
+        num1, num2 = num1[::-1], num2[::-1] #reversed
+        for i1 in range(len(num1)):
+            for i2 in range(len(num2)):
+                digit = int(num1[i1]) * int(num2[i2])
+                res[i1 + i2] += digit
+                res[i1 + i2 + 1] += res[i1 + i2] // 10 #multiply("123","456") from 18, 1 is saved in i+1
+                res[i1 + i2] = res[i1 + i2] % 10 # in i, 8 is saved in the i
+        
+        res, beg = res[::-1], 0
+        while beg < len(res) and res[beg] == 0:
+            beg += 1
+        res = map(str, res[beg:])
+        return "".join(res)
