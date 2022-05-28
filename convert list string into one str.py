d638618946b9eb84f4f68a42c9ce07da9b7880a9
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-28 16:41:29 -0400

    Update convert list string into one str.py

--- convert list string into one str.py
+++ convert list string into one str.py
@@ -9,10 +9,11 @@ condition3 abc; a"bc; d;fc
 def 
 convert array of strings into one string python
 
+Version1
 '''
-
+output = 'abc;b""cd;"e;fg"'
 array = ['abc', 'b"cd', 'e;fg']
-s='a"bc'
+
 res=[]
 res_s=""
 
@@ -28,6 +29,32 @@ def converts(array):
             if s2 ==';':
                 s1='"' + str(s1) + '"'
         res_s+= s1 + ";"
+    res_s = res_s[:-1]   
+        
+    return res_s
+
+converts(array)
+
+
+'''
+version2
+'''
+array = ['abc', 'b"cd', 'e;fg']
+def converts(array):
+    
+    res=[]
+    res_s=''
+    
+    
+    for i, s1 in enumerate(array):
+        for j, s2 in enumerate(s1):
+            if s2 ==';':
+                s1='"' + s1 + '"'
+            if s2 =='"':
+                s1=s1.replace('"','""')
+
+        res_s+= s1 + ";"
+
     res_s = res_s[:-1]   
         
     return res_s
