03768a3600c28bd04f43ea3c6b3a51c4b33b9b08
park-kook <89983499+park-kook@users.noreply.github.com>
2022-06-04 21:13:59 -0400

    Update lru cache class.py

--- lru cache class.py
+++ lru cache class.py
@@ -36,40 +36,49 @@ input =[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
 output = [null, null, null, 1, null, -1, null, -1, 3, 4]
 
 
+
 class Node:
     def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
-        
+        self.key, self.val = key, val #store both key and val
+        self.prev = self.next = None #next set to none means the tail of the linked list, and prev set to
+        # none then this indicates that the node is the head of the linked list. 
+
+    def __str__(self):
+        return '->'.join([str(Node) for Node in self]) 
+       
 class LRUCache:
     def __init__(self, capacity:int):
 #    def __init__(self, capacity):
         self.cap = capacity
-        self.cache ={} #map key to node
+        self.cache ={} #map key to node, value is the pointer
         
         #left = LRU, right = most recent
-        self.left, self.right = Node(0,0), Node(0,0)
-        self.left.next, self.right.prev = self.right, self.left #swap when touch the most recent
+        self.left, self.right = Node(0,0), Node(0,0) #these are dummy nodes for least and most recent nodes
+         #if linked list has no right and left then it means
+        # it's empty and therefore the node to be added will be both the head and the tail of the LL
+        self.left.next, self.right.prev = self.right, self.left #it will be used insert function below
         
-        #remove node from list
+        #remove node from list - 1 helper function 
     def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-        #prev, nxt are pointers
+        prev, nxt = node.prev, node.next #move previous pointer to the middle into the far right
+        #the input node in the middle setting up node.prev = prev
+        prev.next, nxt.prev = nxt, prev #update: reconnect tail and head by skipping the middle node
+        #prev, nxt are nodes
+        #prev.next and nxt.prev are pointers
         #remove the middle of them
         
-        #insert node at right
+        #insert node at right most position - 2 helper function 
     def insert(self,node):
-        prev, nxt = self.right.prev, self.right
+        prev, nxt = self.right.prev, self.right #starting from far right
         prev.next = nxt.prev = node #node will be inserted in the middle of them
-        node.next, node.prev = nxt, prev
+        node.next, node.prev = nxt, prev #reconnect the pointer tothe middle of prev and right
         
         
     def get(self,key:int)->int:
 #    def get(self,key):
         if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
+            self.remove(self.cache[key]) #update the most update means delete[1,1]
+            self.insert(self.cache[key]) #update the most update means insert[1,1] in right position
             return self.cache[key].val
         
         return -1
@@ -77,15 +86,15 @@ class LRUCache:
     def put(self, key:int, value: int)->None:   
 #    def put(self, key, value):
         if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key,value)
-        self.insert(self.cache[key])
+            self.remove(self.cache[key]) # if key in the cache, the remove the key first in the cache
+        self.cache[key] = Node(key,value) #we put new node in the hashmap pointing to this node
+        self.insert(self.cache[key]) #and then this node insert in our list
         
         if len(self.cache) >self.cap:
             #remove from the linked list and delete the LRU from the hashmap
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
+            lru = self.left.next #accoridng to the above
+            self.remove(lru) # first, this removes from the linked list
+            del self.cache[lru.key] #and also delete this in the hash map
 
 cache = None
 cache = LRUCache(2)
