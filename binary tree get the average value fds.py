a6f016366974002c881bfb4b1added317419d5f3
park-kook <89983499+park-kook@users.noreply.github.com>
2022-06-16 10:59:17 -0400

    Update binary tree get the average value fds.py

--- binary tree get the average value fds.py
+++ binary tree get the average value fds.py
@@ -123,6 +123,54 @@ avg_by_depth(node)
 output = [4.0, 8.0, 6.0, 6.0, 2.0]
 
 
+'''
+import collections
+import heapq
+from collections import deque
+from collections import defaultdict
+from collections import deque
+'''
+Merge k sorted arrays (Different Sized Arrays)
+           3
+        /    \ 
+       9      20
+            /   \
+            15   7
+'''
+class Node: 
+    def __init__(self, val=0):
+        self.val = val
+        self.left = None
+        self.right = None
+        
+        
+def levelOrder(root):
+    res = []
+    q = collections.deque()
+    q.append(root)
+    
+    while q:
+#        qLen = len(q)
+        level = [] # each level: top node, first floor, second floor
+        for i in range(len(q)):
+            node = q.popleft()
+            if node:
+                level.append(node.val)
+                q.append(node.left)
+                q.append(node.right)
+                
+        if level:
+            res.append(level)
+    return res
+
+root = None
+root = Node(3)
+root.left = Node(9)
+root.right = Node(20)
+root.right.left = Node(15)
+root.right.right = Node(7)
+levelOrder(root)
+output = [[3], [9, 20], [15, 7]]
 
 '''
 average sum by level order using queue BDF
