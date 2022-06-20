8640551ac0c13f3f380084504b81065620c3d895
park-kook <89983499+park-kook@users.noreply.github.com>
2022-06-20 17:51:08 -0400

    Update top k frequent elements.py

--- top k frequent elements.py
+++ top k frequent elements.py
@@ -27,3 +27,34 @@ def topKFrequent(nums,k):
 #                print(res)
                 return res
 topKFrequent(nums,k)
+
+
+###anotherersion
+from collections import defaultdict
+def topKFrequent(nums,k):
+    count = {}
+    freq = defaultdict(list)
+#    count2 = {i:[] for i in range(len(nums))}
+#    freq = [[] for i in range(len(nums)+1)]
+    
+    for n in nums:
+        count[n] = 1+ count.get(n,0) #{1: 3, 2: 2, 3: 1}
+    for n,c in count.items():
+        freq[c].append(n)
+#        freq[c].append(n) #[[], [3], [2], [1], [], [], []] most frequent sotred in index
+        
+    res = []
+#    for i in range(len(freq)-1,0,-1): #so inversed order
+    for i in range(len(freq),0,-1): #so inversed order
+        print(i)
+        for n in freq[i]:
+            res.append(n)
+            if len(res) ==k:
+#                print(res)
+                return res
+            
+#    for i in count:
+#        if count[i]>=k:
+#            res.append(i)
+            
+topKFrequent(nums,k)
