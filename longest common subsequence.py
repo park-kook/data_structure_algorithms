0faa338ac9ffeb55ccf90f081f3216bc07d429fc
park-kook <89983499+park-kook@users.noreply.github.com>
2022-06-20 18:10:01 -0400

    Update longest common subsequence.py

--- longest common subsequence.py
+++ longest common subsequence.py
@@ -12,6 +12,23 @@ text1 = "abcde"
 text2 = "ace"
 output = 3
 1+window.get(s[r],0)
+
+
+#O(n*m)
+def longestCommonSubsequence(text1, text2):
+    dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)] #m+1 * n+1 matrix
+    
+    for i in range(len(text1)-1, -1, -1):
+        for j in range(len(text2)-1, -1, -1):
+            if text1[i] == text2[j]:
+                dp[i][j] = 1+dp[i+1][j+1] # it matches, goes to the diagonal
+            else: 
+                dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+    return dp[0][0]
+longestCommonSubsequence(text1, text2)    
+
+
+
 def longestCommonSubsequence(text1, text2):
     dict_1={}
     
@@ -24,18 +41,3 @@ def longestCommonSubsequence(text1, text2):
             dict_1[t2] =1+ dict_1.get(t2,0)
     return sum(dict_1.values())
 longestCommonSubsequence(text1, text2)
-
-
-#O(n*m)
-def longestCommonSubsequence(text1, text2):
-    dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
-    
-    for i in range(len(text1)-1, -1, -1):
-        for j in range(len(text2)-1, -1, -1):
-            if text1[i] == text2[j]:
-                dp[i][j] = 1+dp[i+1][j+1] # it matches, goes to the diagonal
-            else: 
-                dp[i][j] = max(dp[i][j+1], dp[i+1][j])
-    return dp[0][0]
-longestCommonSubsequence(text1, text2)    
-    
