b89ed88c4a6fbb87f95779648830c603010d3144
park-kook <89983499+park-kook@users.noreply.github.com>
2022-06-20 15:26:21 -0400

    Update valid graph tree.py

--- valid graph tree.py
+++ valid graph tree.py
@@ -3,6 +3,13 @@
 Graph Valid Tree
 Given n ndoes labeled from 0 to n01 and a list of undirected edges (each h edge is a pair of nodes), 
 write a function to check whether these edges make up a valid tree. 
+1) There is no cycle. 
+2) The graph is connected.
+0 1, 1 2, 2 3, 0 2 
+true 
+
+0 1, 1 2, 2 3 
+false
 '''
 n = 5
 edges = [[0,1],[0,2],[0,3],[1,4]]
