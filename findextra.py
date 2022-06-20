f2a1e00f6e333fab797c333846e09b9a75d6841d
park-kook <89983499+park-kook@users.noreply.github.com>
2022-06-20 15:45:41 -0400

    Create findextra.py

---
+++ findextra.py
@@ -0,0 +1,33 @@
+
+A = [ 10, 15, 5 ]
+B = [ 10, 100, 15, 5 ]
+output = 100
+
+A = [2, 4, 6, 8,  10, 12, 13]
+B = [2, 4, 6, 8, 10, 12]
+
+output=13
+
+def findExtra(A, B):
+    if len(A) <len(B):
+        A,B=B,A
+    for i in range(len(B)):
+        if A[i] != B[i]:
+            return A[i]
+                     
+    return A[len(A)-1]
+  
+  
+  
+  def extraElement(A, B):
+ 
+    # To store the result
+    ans = 0
+ 
+    # Find the XOR of all the element
+    # of array A[] and array B[]
+    for i in range(len(A)):
+        ans ^= A[i];
+    for i in range(len(B)):
+        ans ^= B[i];
+ 
