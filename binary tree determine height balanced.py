a918f6f022f4c194edde2a30201f6b9e522cbd03
park-kook <89983499+park-kook@users.noreply.github.com>
2022-07-25 22:13:39 -0400

    Update binary tree determine height balanced.py

--- binary tree determine height balanced.py
+++ binary tree determine height balanced.py
@@ -41,3 +41,32 @@ root.left.right = Node(5)
 root.right.left = Node(6)
 root.left.left.left = Node(7)
 isBalanced(root)
+
+
+
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def height(root):
+    if root is None:
+        return 0
+    return max(height(root.left), height(root.right)) + 1
+
+def isBalanced(rott):
+    #base condition
+    if root is None:
+        return True
+    #for left and right subtree height
+    lh = height(root.left)
+    rh = height(root.right)
+    
+    # allowed values for (lh - rh) are 1, -1, 0
+    if (abs(lh-rh) <=1 ) and isBalanced(root.left) is True and isBalanced(root.right) is True:
+        return True
+    
+    #if we reach here means tree is not
+    #height-balanced tree
+    return False
