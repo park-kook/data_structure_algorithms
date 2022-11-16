c81371f7981b76f1ca8f24c69e9e71f9426417d5
park-kook <89983499+park-kook@users.noreply.github.com>
2022-11-16 07:54:37 -0500

    Update [i]_JPMC_valie_paraenthes.py

--- [i]_JPMC_valie_paraenthes.py
+++ [i]_JPMC_valie_paraenthes.py
@@ -37,3 +37,47 @@ valid('()')
 valid('(())')
 valid('((())')
 valid('(()))')
+
+'''
+JPMC interview
+'''
+# Question: Given a string s containing just the characters "(", ")", "{", "}", "[" and "]", 
+# determine if the input string is valid. An input string is valid IF:
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Examples:
+# Input: s = "()", Output: true
+# Input: s = "(]", Output: false 
+# Input: s = "{[]}", Output: true
+
+def valid_p(s):
+    init_valid = set('([{')
+    #check_set = set('(){}[]')
+    match = set([('(',')'),('[',']'),('{','}')])
+    result = []
+    for i in s:
+    #    if i not in check_set:
+    #        continue
+        if i in init_valid:
+            result.append(i)
+        else:
+            if len(result) == 0:
+                return False
+
+            second = result.pop()
+            if (second,i) in match: 
+                return True
+            else: 
+                return False
+
+    return len(result) ==0
+
+a ={1,2,3,4,5,6....... n}
+b = [1,2,3,4,5,6]
+0, 1,,,,, , n-1
+for n in range(len(a)):
+    print(a[n]) 
+
+    [x for in range(10)]
+    {x:None for in range(10)}
+    
