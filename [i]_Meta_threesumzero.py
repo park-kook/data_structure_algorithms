083e05cd331abb53d2ba4d41b1a643593f0c3c9f
park-kook <89983499+park-kook@users.noreply.github.com>
2022-11-17 16:18:00 -0500

    Update [i]_Meta_threesumzero.py

--- [i]_Meta_threesumzero.py
+++ [i]_Meta_threesumzero.py
@@ -1,6 +1,42 @@
 '''
 #meta mock interview
 '''
+    def threeSum(nums):    
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+    # Now fix the first element
+    # one by one and find the
+    # other two elements
+        N, result = len(nums), []
+        for i in range(len(nums)):
+            if nums[i] == nums[i-1]: # prevent from dulplicate set, shold not go through if previous value is the same
+                continue 
+            print(i)
+
+            target = nums[i]*-1 #reduce to two sum proble, a+b = -c
+            l= i+1 #index of the first element in the remining elements- not duplicate
+            r =len(nums)-1 # index of the last element
+            while l<r:
+                if nums[l]+nums[r] == target:
+                    result.append([nums[i], nums[l], nums[r]])
+                    l = l+1
+                    while l<r and nums[l] == nums[r-1]:
+                        l = l+1
+                elif nums[l] + nums[r] < target:
+                    l += 1
+                else:
+                    r -= 1
+        return result
+    
+
+threeSum([-1,0])        
+threeSum([-1,0,1,2,-1,-4])    
+threeSum([-2,-2,0,0,2,2])  
+
+
 
 def threesumzero(arr):
     arr.sort()
