9230c9c71cc6a1f129018909fde5f21ad8884555
park-kook <89983499+park-kook@users.noreply.github.com>
2023-01-31 09:20:01 -0500

    Update [I]_jpmc_color_picked_frequency.py

--- [I]_jpmc_color_picked_frequency.py
+++ [I]_jpmc_color_picked_frequency.py
@@ -33,9 +33,46 @@ def rand_pick(a):
 
 
 
-    roll = random.random()*total
+#     roll = random.random()*total
+    roll = random.randint(1,10)
     for i, cum_w in enumerate(cum_freq):
         if roll < cum_w:
             return color_l[i]
 rand_pick(a)
 
+
+
+
+
+
+
+
+class Solution:
+
+    def __init__(self, w: List[int]):
+        """
+        :type w: List[int]
+        """
+        self.prefix_sums = []
+        prefix_sum = 0
+        for weight in w:
+            prefix_sum += weight
+            self.prefix_sums.append(prefix_sum)
+        self.total_sum = prefix_sum
+
+    def pickIndex(self) -> int:
+        """
+        :rtype: int
+        """
+        target = self.total_sum * random.random()
+        # run a linear search to find the target zone
+        for i, prefix_sum in enumerate(self.prefix_sums):
+            if target < prefix_sum:
+                return i
+            
+import random
+#Choose 5 elements from the list with different probability
+sampleList = [10, 20, 30, 40]
+weight = [10,20,30,40]
+x = random.choices(sampleList, weights = weight)
+print(x)
