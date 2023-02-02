dadd5e562d8d7e1c494cb0393b2a455120d283c1
park-kook <89983499+park-kook@users.noreply.github.com>
2023-02-02 17:20:22 -0500

    Update [i]_airbnb_Alien Dictionary.py

--- [i]_airbnb_Alien Dictionary.py
+++ [i]_airbnb_Alien Dictionary.py
@@ -1,44 +1,46 @@
-'''
-#Input: words[] = {“baa”, “abcd”, “abca”, “cab”, “cad”} Output: Order of characters is ‘b’, ‘d’, ‘a’, ‘c’
 
-# You have found a list of alien words. You know for a fact that words in this list are sorted, 
-#but you don’t know what the alphabet is. You would like to reconstruct the original alphabet in such a way, 
-that the ordering between letters would explain the sorting of the list.
+    
+   ######################################################################################
+
 '''
-def alienOrder(self, words: List(str))->str:
-    adj = {c:set() for w in words for c in w}
-    # Build a graph for topological sort
-    for i in range(len(words) - 1):
-        w1, w2 = words[i], words[i+1]
-        min_len = min(len(w1), len(w2))
-        if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]
-            # Invalid case. w1 should be after w2
-            return ""
-        for j in range(min_len):
-            if w1[j] != w2[j]:
-                adj[w1[j]].add(w2[j])
-                break
-    visit = {}
-    res = []
-    # post-order dfs and check if there is a circle
-    def dfs(c):
-        if c in visit:
-            # there is a circle
-            return True
-        visit[c] = True
-        for n in adj[c]:
-            if dfs(n):
-                return True
-        res.append(c)
-        visit[c] = False
-    for c in adj:
-        if dfs(c):
-            # Invalid case
-            return ""
-    returun "".join(res[::-1])
-    
-    
-    
+Alien Dictionary - Hard
+There is a new alien language that uses the English alphabe. However, 
+the order among the letters is unknown to you. 
+You are given a list of strings words from the alien langugage's dictionary, where the strings in words
+are sorted lexicographically by the rules of this new language. 
+
+Return a string of the unique letters in the new alien lagngugae sorted in lexicographically
+increasing order by the new langugage's rules. If there is no solution, return "". If there are 
+multiple solutions, return any of them. 
+
+A string s is smaller than a string t if at the first letter where they differ, the letter in s
+comes before the letter in t in the alien language. If the first min(s.length, t.length) letteres are 
+the same, then s is smaller if and only if s.length < t.length
+
+words = ["wrt", "wrf", "er", "ett", "rftt"]
+output = "wertf"
+w->e->r->t->f
+
+t->f
+w->e
+r->t
+e->r
+
+words2=["A", "BA", "BC", "C"]
+A->B
+A->C
+B->C
+
+post order dfs
+A->B->C
+A->C
+not store A because this is postorder, A->C, there is no further children
+now store C in the output, then back to A and go to B (this is postorder, we have to go to all decendent
+and print), and C has been already processed,it means gone to the outputlist, so B print because there is no
+decendent, and then A print
+CBA
+we need to reverse order. ABC
+''' 
     
     
     
@@ -85,47 +87,7 @@ output[0][0]
 
 
 
-######################################################################################
 
-'''
-Alien Dictionary - Hard
-There is a new alien language that uses the English alphabe. However, 
-the order among the letters is unknown to you. 
-You are given a list of strings words from the alien langugage's dictionary, where the strings in words
-are sorted lexicographically by the rules of this new language. 
-
-Return a string of the unique letters in the new alien lagngugae sorted in lexicographically
-increasing order by the new langugage's rules. If there is no solution, return "". If there are 
-multiple solutions, return any of them. 
-
-A string s is smaller than a string t if at the first letter where they differ, the letter in s
-comes before the letter in t in the alien language. If the first min(s.length, t.length) letteres are 
-the same, then s is smaller if and only if s.length < t.length
-
-words = ["wrt", "wrf", "er", "ett", "rftt"]
-output = "wertf"
-w->e->r->t->f
-
-t->f
-w->e
-r->t
-e->r
-
-words2=["A", "BA", "BC", "C"]
-A->B
-A->C
-B->C
-
-post order dfs
-A->B->C
-A->C
-not store A because this is postorder, A->C, there is no further children
-now store C in the output, then back to A and go to B (this is postorder, we have to go to all decendent
-and print), and C has been already processed,it means gone to the outputlist, so B print because there is no
-decendent, and then A print
-CBA
-we need to reverse order. ABC
-'''
 words=["A", "BA", "BC", "C"]
 output = "ABC"
 words = ["wrt", "wrf", "er", "ett", "rftt"]
@@ -154,17 +116,17 @@ def alienOrder(words):
         if c in visit:
             return visit[c]
         visit[c] = True #Visit = {A:'True', B:'True', C:'Ture}
-        
+# A->B
+# A->C
+# B->C        
         for neighbor in adj[c]: #A-> neighbor: B, C
 #            print(neighbor) #A->B and C, B->C,
             if dfs(neighbor):
                 return True
-#A->B
-#A->C
-#B->C  
+
 #[C]->[C,B]->[C,B,A]
-        visit[c] = False #{'A': False, 'B': True, 'C': True}
-        res.append(c) #res = ['A']
+        visit[c] = False #{'C': False, 'A': True, 'B': True}
+        res.append(c) #res = ['C']
         
     for c in adj:#A, B, C
 #        print(c)
