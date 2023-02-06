6a5cb299f67e1f300ae31c60b06b2f66aa5dd6b2
park-kook <89983499+park-kook@users.noreply.github.com>
2023-02-06 15:19:04 -0500

    Update [i]_amazon_longest palindrome substring.py

--- [i]_amazon_longest palindrome substring.py
+++ [i]_amazon_longest palindrome substring.py
@@ -27,7 +27,8 @@ def longestPalindrome(s):
     
 
       
-#output even length palindromes
+#output even length palindromes for s = "cabbad" output = "abba"
+#that's why l = i, r = i+1 as a strating point, and then expand l-=1 and r+=1
   
     l,r =i,i+1
     while l>=0 and r<len(s) and s[l] ==s[r]:
@@ -41,9 +42,13 @@ def longestPalindrome(s):
       
       
 #for output odd length palindromes, the following script should be additionally added
+#s = "cababad"
+#Output: "ababa"
+#this example do not go through the script for even length palindromes above. Only second script below go through
+# for output odd length palindromes . 
 
 #The edge case (and reason for the seemingly redundant code) is that the above grabs 
-#odd length palindromes by starting with 1 index and expanding outwards. 
+#odd length palindromes by starting with 1 same index and expanding outwards. 
 #You can grab the even case by doing the while loop again, 
 #but initializing the pointers to two indices next to each other rather than the same.    
     l,r = i,i
