660d9b818ca6f9b2eb05720aae872f357581e335
park-kook <89983499+park-kook@users.noreply.github.com>
2023-03-06 11:34:59 -0500

    Update [i]_meta_cd_converting.py

--- [i]_meta_cd_converting.py
+++ [i]_meta_cd_converting.py
@@ -11,21 +11,21 @@ cd converting
 func(cwd, cd) -> new_cwd
 
 
-cwd="/"
-cd="foo"
+cwd="/" #['',''] after split '/'
+cd="foo" #['foo']
 
-cwd="/foo/bar"
-cd="../../../../.."
+cwd="/foo/bar" #['','foo','bar']
+cd="../../../../.." #['..','..','..','..','..']
 
-cwd="/baz"
-cd="/bar"
+cwd="/baz" #['','baz']
+cd="/bar" #['','bar']
  
-cwd="/x/y"
-cd="../p/../q"
+cwd="/x/y" #['','x','y']
+cd="../p/../q" #['..','p','..','q']
 
 
-cwd="/x/y"
-cd="/p/./q"
+cwd="/x/y" #['','x','y']
+cd="/p/./q" #['','p','.','q']
 '''
 def fund(cwd,cd):
   cwd_l = cwd.split('/')
@@ -49,7 +49,27 @@ def fund(cwd,cd):
 func(cwd,cd)
     
     
+def fund(cwd,cd):
+  cwd_l = cwd.split('/')
+  cd_l = cd.split('/')
+  res =''
+  if cd_l[0]=='': #if cd starts from / then delete all currnet path. 
+    cwd_l=['']
+  if cwd =='/': #cwd_l = ["",""] then second missing needs to be removed. 
+    cwd_l.pop()
     
+  for i in range(len(cd_l)):
+    if len(cwd_l) >1 and cd_l[i] =='..':
+      cwd_l.pop()
+      #continue #cd_l == '..' remove in the loop      
+    if cd_l[i] =='.':
+      continue
+    elif cd_l[i] = !='..' and cd_l[!='':
+      cwd_l.append(cd_l[i])
+  res = '/'.join(cwd_l)
+  return res if res!='' else '/'
+
+func(cwd,cd)    
     
   
   
