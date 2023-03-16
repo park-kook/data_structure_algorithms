427052e855b086b616e709e50de23a95a0c08b39
park-kook <89983499+park-kook@users.noreply.github.com>
2023-03-16 08:39:40 -0400

    Update [i]_meta_convert_3condition_array.py

--- [i]_meta_convert_3condition_array.py
+++ [i]_meta_convert_3condition_array.py
@@ -1,6 +1,18 @@
+'''
 
+a = [abc, a"bc, d;fc]
 
+condition1 if string has " in the one of the strings, then ""
+condition2 if string has ; in the one of the strings, then "sting"
+condition3 abc; a"bc; d;fc
 
+def 
+convert array of strings into one string python
+
+'''
+
+array = ['abc', 'b"cd', 'e;fg']
+s='a"bc'
 
 def converts(array):
   res=[]
