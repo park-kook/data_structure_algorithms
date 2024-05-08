77455c09ff528493597db5092bd18c75370d1f9c
park-kook <89983499+park-kook@users.noreply.github.com>
2024-05-08 16:42:56 -0400

    Update [akshay]_Trapping Rain Water.py

--- [akshay]_Trapping Rain Water.py
+++ [akshay]_Trapping Rain Water.py
@@ -1,3 +1,8 @@
+'''
+Given an array of N non-negative integers arr[] representing an elevation map 
+where the width of each bar is 1, compute how much water it is able to trap after raining.
+'''
+
 def findWater(arr, n): 
   
     # left[i] contains height of tallest bar to the 
