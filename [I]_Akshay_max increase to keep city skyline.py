9b4c1774173f2e5a711f9ea6f68afd87e2ee60ef
park-kook <89983499+park-kook@users.noreply.github.com>
2024-05-09 12:11:15 -0400

    Update and rename [I]_max increase to keep city skyline.py to [I]_Akshay_max increase to keep city skyline.py

--- [I]_Akshay_max increase to keep city skyline.py
+++ [I]_Akshay_max increase to keep city skyline.py
@@ -18,6 +18,8 @@ Return the maximum total sum that the height of the buildings can be increased
 by without changing the city's skyline from any cardinal direction.
 '''
 
+grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+#Output =  35
 
 class Solution(object):
     def maxIncreaseKeepingSkyline(self, grid):
